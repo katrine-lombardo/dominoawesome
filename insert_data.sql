7506fe11b5f96e201b8e8747a0b3a6c5f81deb99
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-03-08 17:56:26 -0500

    Insert filament data

--- insert_data.sql
+++ insert_data.sql
@@ -1,3 +1,8 @@
-INSERT INTO filaments (color, grams, unit_price, material) VALUES (
-    'Black', 1000, 14.00, 'PLA'
-);
+INSERT INTO filaments (id, color, grams, unit_price, material) VALUES
+    (0, 'Black', 1000, 14.00, 'PLA'),
+    (1, 'Red', 1000, 23.99, 'PLA'),
+    (2, 'White', 1000, 15.00, 'PLA'),
+    (3, 'Blue', 1000, 18.00, 'PLA'),
+    (4, 'Light Blue', 1000, 20.99, 'PLA');
+
+
